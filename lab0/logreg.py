f69ac28b5c252348aacc1536de8bb1894f5fda69
Magda Radić <magdina.radic@gmail.com>
2024-03-24 18:17:35 +0100

    rename folder

--- lab0/logreg.py
+++ lab0/logreg.py
@@ -4,3 +4,19 @@ import binlogreg as binlr
 
 def binlogreg_decfun(w, b):
     return lambda X: binlr.binlogreg_classify(X, w, b)
+
+def logreg_train(X, Y_):
+    """
+    Trains a multi-class logistic regression model
+
+    Args:
+        X: ndarray NxD input data matrix,
+        Y_: ndarray Nx1 target variable,
+
+    Returns:
+        W: ndarray CxD weight matrix,
+        b: ndarray Cx1 bias vector,
+
+    Note:
+        C = number of classes = MAX(Y_) + 1.
+    """
