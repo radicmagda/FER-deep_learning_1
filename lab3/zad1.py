00d41193ebefeee88b2ee31262893f566e5586ce
Magda Radić <magdina.radic@gmail.com>
2024-05-20 16:35:38 +0200

    NLPDataset, Instance

--- lab3/zad1.py
+++ lab3/zad1.py
@@ -3,35 +3,46 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Instance:
+    text: list[str]
+    label: str
+
+class Vocab:
+    def __init__(self, frequencies, max_size=-1, min_freq=0):
+        self.vf=frequencies
+
+    def encode(source):
+        #must return a tensor of numerical values of words
+        return torch.tensor(0)
 
 class NLPDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
-        self.data_frame = pd.read_csv(csv_file)
-        self.transform = transform
-        
-        # Assuming the last column is the target and the rest are features
-        self.features = self.data_frame.iloc[:, :-1].values
-        self.labels = self.data_frame.iloc[:, -1].values
-        
-        # Standardize the features (optional)
-        #.. finish implementation
+    def __init__(self, csv_file, text_vocab:Vocab, label_vocab:Vocab):
+        data_frame = pd.read_csv(csv_file)
+        self.instances=[Instance(text.split(), label) for text, label in data_frame.values] #lista Instance-ova
+
+        self.text_vocab=text_vocab
+        self.label_vocab=label_vocab
+
 
     def __len__(self):
-        return len(self.data_frame)
+        return len(self.instances)
 
     def __getitem__(self, idx):
-        sample = {'features': torch.tensor(self.features[idx], dtype=torch.float32),
-                  'labels': torch.tensor(self.labels[idx], dtype=torch.float32)}
-        
-        if self.transform:
-            sample = self.transform(sample)
-        
-        return sample
+        #vraca: numerikalizirani tekst (torch.tensor 1.reda/listu), numerikaliziranu labelu (torch.tensor 0.reda/skalar)
+        inst=self.instances[idx]
+        text=inst.text
+        label=inst.label
+        return self.text_vocab.encode(text), self.
 
 # Instantiate the datasets
-train_dataset = NLPDataset('data/sst_train_raw.csv')
-valid_dataset = NLPDataset('data/sst_valid_raw.csv')
-test_dataset = NLPDataset('data/sst_test_raw.csv')
+train_dataset = NLPDataset('lab3/data/sst_train_raw.csv')
+valid_dataset = NLPDataset('lab3/data/sst_valid_raw.csv')
+test_dataset = NLPDataset('lab3/data/sst_test_raw.csv')
 
 # Create data loaders
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
