921f49421b9a0b5cea9668caad04ae1bf9935587
Magda Radić <magdina.radic@gmail.com>
2024-05-24 21:34:44 +0200

    zad2 BNReluConv

---
+++ lab4/zad2.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class _BNReluConv(nn.Sequential):
+    def __init__(self, num_maps_in, num_maps_out, k=3, bias=True):
+        super(_BNReluConv, self).__init__()
+        # YOUR CODE HERE
+        self.append(torch.nn.BatchNorm2d(num_maps_in, affine=bias))
+        self.append(torch.nn.ReLU())
+        self.append(torch.nn.Conv2d(num_maps_in, num_maps_out, kernel_size=k))
+
+class SimpleMetricEmbedding(nn.Module):
+    def __init__(self, input_channels, emb_size=32):
+        super().__init__()
+        self.emb_size = emb_size
+        # YOUR CODE HERE
+
+    def get_features(self, img):
+        # Returns tensor with dimensions BATCH_SIZE, EMB_SIZE
+        # YOUR CODE HERE
+        x = ...
+        return x
+
+    def loss(self, anchor, positive, negative):
+        a_x = self.get_features(anchor)
+        p_x = self.get_features(positive)
+        n_x = self.get_features(negative)
+        # YOUR CODE HERE
+        loss = ...
+        return loss
