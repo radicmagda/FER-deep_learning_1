76d23ebec2c5a705d63a7c551b857e4e0c94ac31
Magda Radić <magdina.radic@gmail.com>
2024-05-26 01:00:59 +0200

    fkž

--- lab4/zad2.py
+++ lab4/zad2.py
@@ -6,12 +6,13 @@ import torch.nn.functional as F
 class _BNReluConv(nn.Sequential):    
     def __init__(self, num_maps_in, num_maps_out, k=3, bias=True):
         """
-        num_maps_in: number of input channels for the conv layer
+        num_maps_in: number of input channels for the conv layer, also number of input and output channels for the BatchNorm2d layer
         num_maps_out: number of output channels for the conv layer/ number of filters of the conv layer
         k: kernel size of the conv layer
         bias: if True, the batch normalization layer will have learnable affine parameters (scale and shift).
         """
         super(_BNReluConv, self).__init__()
+        # YOUR CODE HERE
         self.append(torch.nn.BatchNorm2d(num_maps_in, affine=bias))
         self.append(torch.nn.ReLU())
         self.append(torch.nn.Conv2d(num_maps_in, num_maps_out, kernel_size=k))
@@ -21,11 +22,26 @@ class SimpleMetricEmbedding(nn.Module):
         super().__init__()
         self.emb_size = emb_size
         # YOUR CODE HERE
+        self.margin = 1
+        self.unit_1 = _BNReluConv(input_channels, emb_size)
+        self.maxpool_1 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.unit_2 = _BNReluConv(emb_size, emb_size)
+        self.maxpool_2 = torch.nn.MaxPool2d(kernel_size=3, stride=2)
+        self.unit_3 = _BNReluConv(emb_size, emb_size)
+        self.global_avg = nn.AvgPool2d(kernel_size=2)
+        
 
     def get_features(self, img):
         # Returns tensor with dimensions BATCH_SIZE, EMB_SIZE
+        # img is tensor of dimenstions BATCH_SIZE, C, H, W 
         # YOUR CODE HERE
-        x = ...
+        x = self.unit_1(img)
+        x = self.maxpool_1(x)
+        x = self.unit_2(x)
+        x = self.maxpool_2(x)
+        x = self.unit_3(x)
+        x = self.global_avg(x)
+        x = x.reshape((img.size(dim=0), self.emb_size))
         return x
 
     def loss(self, anchor, positive, negative):
