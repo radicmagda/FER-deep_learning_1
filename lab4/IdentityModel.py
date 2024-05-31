a0436cd323e4be58f3c3f3d549709656ba990251
Magda Radić <magdina.radic@gmail.com>
2024-05-31 14:45:29 +0200

    identity model

---
+++ lab4/IdentityModel.py
@@ -0,0 +1,14 @@
+import torch
+
+class IdentityModel(nn.Module):
+    def __init__(self):
+        super(IdentityModel, self).__init__()
+
+    def get_features(self, img):
+        """
+        obična vektorizacija slike u R^(CxHxW)
+        """
+        # YOUR CODE HERE
+        # iz (B, C, H, W) u (B, C*H*W) tj "peglanje" slike
+        feats = torch.flatten(img, start_dim=1) # start_dim=1 jer zelimo ocuvati dimenziju batcha
+        return feats
